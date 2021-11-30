350474a386645dac314c6e2387ba383bb35c9d3e
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-30 20:02:22 +0530

    Roll Die to get Number

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -3,8 +3,33 @@ class Employeewage
 {
     public static void Main()
     {
+        //UC1
         UC1_Start uc1 = new UC1_Start();
-        uc1.Start();
+
+        //UC2
+        UC2_ToGetNum0_6 uc2 = new UC2_ToGetNum0_6();
+       
+
+        bool isRun = true;
+        while (isRun)
+        {
+            Console.WriteLine("\nSelect UC Number:\n" +
+                "1.Start \n" +
+                "2.Rolls the die\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    uc1.Start();
+                    break;
+                case 2:
+                    uc2.GetNumber();
+                    break;
+                default:
+                    isRun = !isRun;
+                    break;
+            }
+        }
 
     }
 }
---
+++ Snake/Snake/UC2_ToGetNum0_6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ public class UC2_ToGetNum0_6
+    {
+    public void GetNumber()
+    {
+        int Player;
+        Random randamnumber = new Random();
+        Player = randamnumber.Next(1, 6);
+        Console.Write("Player Gets the Number is:" + Player+"\n");
+    }
+   
+
+}
+
