bb9f3bb3a4daad942bc95431f7930eaa753aff27
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-01 15:17:17 +0530

    Repeat till the player reaches the winning position 100

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,13 +12,19 @@ class Employeewage
         //UC3
         UC3_CheckOption uc3 = new UC3_CheckOption();
 
+        //UC4
+        UC4_Repeat uc4 = new UC4_Repeat();
+
+
         bool isRun = true;
         while (isRun)
         {
             Console.WriteLine("\nSelect UC Number:\n" +
                 "1.Start \n" +
                 "2.Rolls the die\n" +
-                "3.The Player then checks for a Option.They are No Play, Ladder or Snake\n\n");
+                "3.The Player then checks for a Option.They are No Play, Ladder or Snake\n" +
+                "4.Repeat till the Player reaches the winning position 100." +
+                "\n\n");
 
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -31,6 +37,9 @@ class Employeewage
                     break;
                 case 3:
                     uc3.checkoption();
+                    break;
+                case 4:
+                    uc4.Position();
                     break;
                 default:
                     isRun = !isRun;
---
+++ Snake/Snake/UC4_Repeat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class UC4_Repeat
+    {
+	
+		public void Position()
+		{
+		int No_Play = 1;
+		int Play_Ladder = 2;
+		int Play_Snake = 3;
+		int WinningPosition = 100;
+		int playerPosition = 0;
+		int DiceNum;
+		while (playerPosition <= WinningPosition)
+			{
+				Random rNumber = new Random();
+				DiceNum = rNumber.Next(1, 7);
+				Random rd = new Random();
+				int Option = rd.Next(0, 3);
+			if (Option == No_Play)
+			{
+				Console.WriteLine("No play");
+			}
+			else if (Option == Play_Ladder)
+			{
+				playerPosition = playerPosition + DiceNum;
+				Console.WriteLine("Ladder--->Your position is now:" + playerPosition);
+			}
+			else if (Option == Play_Snake)
+			{
+				playerPosition = playerPosition - DiceNum;
+				if (playerPosition < 0)
+				{
+					playerPosition = 0;
+				}
+				Console.WriteLine("Snake--->Your position is:" + playerPosition);
+			}
+		}
+		}
+
+	}
+
