4dbfdbe106d380cf3535c0dcfe83262a1a77cdb9
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-01 14:21:49 +0530

    No Play,Ladder,Snake

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -8,14 +8,18 @@ class Employeewage
 
         //UC2
         UC2_ToGetNum0_6 uc2 = new UC2_ToGetNum0_6();
-       
+
+        //UC3
+        UC3_CheckOption uc3 = new UC3_CheckOption();
 
         bool isRun = true;
         while (isRun)
         {
             Console.WriteLine("\nSelect UC Number:\n" +
                 "1.Start \n" +
-                "2.Rolls the die\n");
+                "2.Rolls the die\n" +
+                "3.The Player then checks for a Option.They are No Play, Ladder or Snake\n\n");
+
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -24,6 +28,9 @@ class Employeewage
                     break;
                 case 2:
                     uc2.GetNumber();
+                    break;
+                case 3:
+                    uc3.checkoption();
                     break;
                 default:
                     isRun = !isRun;
---
+++ Snake/Snake/UC3_CheckOption.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class UC3_CheckOption
+{
+    public void checkoption()
+    {
+        int No_Play = 1;
+        int Play_Ladder = 2;
+        int Play_Snake = 3;
+
+        int Position = 0;
+        Random randomnumber = new Random();
+        int Option = randomnumber.Next(1, 3);
+        int DiceNum = randomnumber.Next(0, 7);
+
+        if(Option == No_Play)
+        {
+            Console.WriteLine("No play");
+        }
+        else if (Option == Play_Ladder)
+        {
+            Console.WriteLine("Ladder--->Your position is now:"+ DiceNum);
+        }
+        else if (Option == Play_Snake)
+        {
+           Console.WriteLine("Snake--->You are still in First position");
+        }
+
+        
+    }
+
+}
+
